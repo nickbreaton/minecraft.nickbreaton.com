e0030bbaf16110efbfd47450283cc6433bdc92cf
nickbreaton <nick@breaton.com>
2020-04-10 13:43:08 -0400

    Add font

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
 import Head from 'next/head'
 import { getStatus } from 'mc-server-status'
 
+const Font = () => (
+  <Head>
+    <link rel="preload" href="MinecraftBody.woff2" as="font" />
+    <style>
+        {`
+          @font-face {
+            font-family: 'MinecraftBody';
+            src: url('MinecraftBody.woff2') format('woff2');
+            font-weight: normal;
+            font-style: normal;
+          }
+
+          body {
+            font-family: MinecraftBody;
+          }
+        `}
+      </style>
+  </Head>
+)
+
 const Home = ({ isOnline }) => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    {isOnline ? '🟢Online' : '🔴Offline'}
+    <Font />
+    <span>{isOnline ? 'Online 🟢' : 'Offline 🔴'}</span>
   </div>
 )
 
